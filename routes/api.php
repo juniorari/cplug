78f7ea36312b7035f76d25bcde8f0f956fff4257
Jose Ari Junior <joseari.jr@gmail.com>
2024-03-08 21:42:48 -0400

    Primeiro envio

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Api\GrupoController;
+use App\Http\Controllers\Api\MoedaController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/moedas/atualiza-banco', [MoedaController::class, 'index'])->name('atualiza-banco');
+Route::get('/moedas/listar', [MoedaController::class, 'listar'])->name('listar');
+
+//Route::get('/listar-grupo', [MoedaController::class, 'listarGrupo'])->name('listarGrupo');
+
+//GET	/sharks	index	sharks.index
+//GET	/sharks/create	create	sharks.create
+//POST	/sharks	store	sharks.store
+//GET	/sharks/{id}	show	sharks.show
+//GET	/sharks/{id}/edit	edit	sharks.edit
+//PUT/PATCH	/sharks/{id}	update	sharks.update
+//DELETE	/sharks/{id}	destroy	sharks.destroy
+Route::resource('grupos', GrupoController::class);
